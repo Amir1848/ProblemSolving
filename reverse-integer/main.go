aafe5da8d613a3d9882e983203fb8c5791af1c6a
Amir Hesari <amir.hesari1848@gmail.com>
2023-03-31 23:12:03 +0430

    fix reverse number for leet code

--- reverse-integer/main.go
+++ reverse-integer/main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"math"
 )
 
+//Given a signed 32-bit integer x, return x with its digits reversed.
+//If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+//site has problem.Looks like the test-values have invalid test cases for x:
+// 1534236469, 1563847412 and -1563847412
+// Because all these numbers are within the required range [-2^31 ; 2^31 -1]
+
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	inputString, err := reader.ReadString('\n')
-	if err != nil {
-		panic(err)
+	fmt.Print(reverse(-547))
+}
+
+func reverse(x int) int {
+	digits := []int{}
+
+	for x != 0 {
+		n := x % 10
+		x = x / 10
+		digits = append(digits, n)
+	}
+	var reversedNumber int
+
+	var tavan = int(math.Pow(10, float64(len(digits)-1)))
+	for _, val := range digits {
+		reversedNumber = reversedNumber + (val * tavan)
+		tavan /= 10
 	}
 
-	startPosition := 0
-	result := []rune{}
-	if inputString[0] == '-' {
-		startPosition = 1
-		result = append(result, '-')
-	}
-
-	for i := len([]rune(inputString)) - 3; i >= startPosition; i-- {
-		result = append(result, []rune(inputString)[i])
-	}
-
-	fmt.Println(string(result))
+	return reversedNumber
 }
